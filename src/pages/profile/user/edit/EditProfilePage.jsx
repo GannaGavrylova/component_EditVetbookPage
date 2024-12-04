08725a14bd102f563d87acff2c61884350700a24
Eugenia Smith <ev-genius4@mail.ru>
2024-12-04 19:00:25 +0100

    Header is added

--- src/pages/profile/user/edit/EditProfilePage.jsx
+++ src/pages/profile/user/edit/EditProfilePage.jsx
@@ -1,7 +1,25 @@
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+import classes from './EditProfilePage.module.css'
+import { FormHeader } from '@shared/components'
+import close from '@/assets/close.svg'
+
 export const EditProfilePage = () => {
+  const { t } = useTranslation()
+
   return (
-    <>
+    <div className={classes.editProfilePage}>
+      <div className={classes.headerContainer}>
+        <FormHeader
+          path={'/profile'}
+          fontSize={36}
+          titleKey={t('burgerMenu.editProfile')}
+        />
+        <Link to={'/profile'}>
+          <img src={close} alt="close" />
+        </Link>
+      </div>
       <p>тут работа кипит</p>
-    </>
+    </div>
   )
 }
