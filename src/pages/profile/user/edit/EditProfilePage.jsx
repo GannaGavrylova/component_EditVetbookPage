ef3c82c137e4b4b67dcbf4009e5fe8101465cc8b
Eugenia Smith <ev-genius4@mail.ru>
2024-12-05 00:49:01 +0100

    The FormHeader component is replaced by PageHeader component

--- src/pages/profile/user/edit/EditProfilePage.jsx
+++ src/pages/profile/user/edit/EditProfilePage.jsx
@@ -1,29 +1,21 @@
 import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
 import classes from './EditProfilePage.module.css'
 import {
-  FormHeader,
+  PageHeader,
   CustomInput,
   CustomButtonSubmit,
   FileUploader,
 } from '@shared/components'
-import close from '@/assets/close.svg'
 
 export const EditProfilePage = () => {
   const { t } = useTranslation()
 
   return (
     <div className={classes.editProfilePage}>
-      <div className={classes.headerContainer}>
-        <FormHeader
-          path={'/profile'}
-          fontSize={36}
-          titleKey={t('burgerMenu.editProfile')}
-        />
-        <Link to={'/profile'}>
-          <img src={close} alt="close" />
-        </Link>
-      </div>
+      <PageHeader
+        pathArrow={'/profile'}
+        titleKey={t('burgerMenu.editProfile')}
+      />
       <FileUploader maxFiles={1} boxSize={160} />
       <form className={classes.editForm} action="" method="post">
         <label className={classes.formLabel}>
