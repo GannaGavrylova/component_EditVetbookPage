8327bab0b4150db46580fae386fc2b20cbec518d
Eugenia Smith <ev-genius4@mail.ru>
2024-12-07 20:50:51 +0100

    Functionality of registering the formData is developed

--- src/pages/profile/user/edit/EditProfilePage.jsx
+++ src/pages/profile/user/edit/EditProfilePage.jsx
@@ -1,45 +1,54 @@
 import { useTranslation } from 'react-i18next'
+import { useForm } from 'react-hook-form'
 import classes from './EditProfilePage.module.css'
-import {
-  PageHeader,
-  CustomInput,
-  CustomButtonSubmit,
-  FileUploader,
-} from '@shared/components'
+import { PageHeader, CustomInput, CustomButtonSubmit, FileUploader } from '@shared/components'
 
 export const EditProfilePage = () => {
   const { t } = useTranslation()
 
+  const { register, handleSubmit, setValue, reset } = useForm({
+    defaultValues: {
+      name: '',
+      phone: '',
+      telegram: '',
+      email: '',
+      file: null,
+    },
+  })
+
+  // const file = watch('file')
+
+  const submitForm = (data) => {
+    alert(JSON.stringify(data, null, 2))
+    reset()
+  }
+
   return (
     <div className={classes.editProfilePage}>
-      <PageHeader
-        pathArrow={'/profile'}
-        titleKey={t('burgerMenu.editProfile')}
-      />
-      <FileUploader maxFiles={1} boxSize={160} />
-      <form className={classes.editForm} action="" method="post">
+      <PageHeader pathArrow={'/profile'} titleKey={t('burgerMenu.editProfile')} />
+      <form className={classes.editForm} onSubmit={handleSubmit(submitForm)} method="post">
+        <FileUploader maxFiles={1} boxSize={160} onUpload={(fileData) => setValue('file', fileData[0])} />
+
         <label className={classes.formLabel}>
           {t('registrationPage.nameLabel')}
-          <CustomInput placeholder={t('userPage.userName')} />
+          <CustomInput placeholder={t('userPage.userName')} {...register('name')} />
         </label>
         <label className={classes.formLabel}>
           {t('registrationPage.phoneLabel')}
-          <CustomInput placeholder={'+382 68 109 018'} />
+          <CustomInput placeholder={'+382 68 109 018'} {...register('phone')} />
         </label>
 
-        <p className={classes.divideText}>
-          {t('userPage.addDataToKeepInTouch')}
-        </p>
+        <p className={classes.divideText}>{t('userPage.addDataToKeepInTouch')}</p>
 
         <label className={classes.formLabel}>
           {t('vetVerificationPage.telegramLabel')}
-          <CustomInput placeholder={'@andrew_yeremin'} />
+          <CustomInput placeholder={'@andrew_yeremin'} {...register('telegram')} />
         </label>
         <label className={classes.formLabel}>
           {t('vetVerificationPage.emailLabel')}
-          <CustomInput placeholder={'andrew_yeremin@email.com'} />
+          <CustomInput placeholder={'andrew_yeremin@email.com'} {...register('email')} />
         </label>
-        <CustomButtonSubmit text={t('userPage.saveChangesBtn')} />
+        <CustomButtonSubmit text={t('userPage.saveChangesBtn')} onClick={handleSubmit(submitForm)} />
       </form>
     </div>
   )
