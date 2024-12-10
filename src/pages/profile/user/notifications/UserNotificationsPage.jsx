422d4d1dae191991554cf20d2d3eac3e17ab9872
Eugenia Smith <ev-genius4@mail.ru>
2024-12-10 19:37:53 +0100

    Toggler component is created, styled, added to the user notification page; a new color variable is added to index.css

--- src/pages/profile/user/notifications/UserNotificationsPage.jsx
+++ src/pages/profile/user/notifications/UserNotificationsPage.jsx
@@ -1,6 +1,6 @@
 import classes from './UserNotificationsPage.module.css'
 import { useTranslation } from 'react-i18next'
-import { PageHeader, CustomCheckbox } from '@shared/components'
+import { PageHeader, Toggler } from '@shared/components'
 
 export const UserNotificationsPage = () => {
   const { t } = useTranslation()
@@ -10,12 +10,12 @@ export const UserNotificationsPage = () => {
       <PageHeader pathArrow={'/profile/settings'} titleKey={t('settings.notifications')} padding="24px 0" fontSize={22} showClose={false} />
       <label className={classes.swichNotifications}>
         {t('E_settingsNotificationProfileUserPage.vetAnswers')}
-        <CustomCheckbox />
+        <Toggler />
       </label>
 
       <label className={classes.swichNotifications}>
         {t('E_settingsNotificationProfileUserPage.serviceNews')}
-        <CustomCheckbox />
+        <Toggler />
       </label>
     </div>
   )
